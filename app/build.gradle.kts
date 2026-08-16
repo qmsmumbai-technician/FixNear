c68936f706dbbd47f9bb661207f1cbe4eb0f85e2
qmsmumbai-technician <qmsmumbai@gmail.com>
2026-08-16 12:02:25 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,14 @@ plugins {
 android {
     namespace = "com.fixnear.app"
     compileSdk = 35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     defaultConfig {
         applicationId = "com.fixnear.app"
         minSdk = 23
